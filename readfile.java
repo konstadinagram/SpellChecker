c2f08741bf091621c2c9fae49819237a97cacf37
aretitsekoura <32673177+aretitsekoura@users.noreply.github.com>
2017-11-21 19:59:58 +0200

    Add files via upload

---
+++ readfile.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.Scanner;
+
+	public class readfile {
+		private Scanner x;
+
+
+	public void openFile() {
+		try {
+			x = new Scanner(new File("english3.txt"));
+		} catch (Exception s) {
+			System.out.println("Could not find file");
+		}
+	}
+
+
+	public void readFile() {
+		while(x.hasNext()) {
+			String a = x.next();
+			String b = x.next();
+			String c = x.next();
+
+		System.out.printf("%s %s %s\n", a,b,c);
+		}
+	}
+
+public void closeFile() {
+	x.close();
+}
+
+}
+
+
+
+
+
+
+
+
+
